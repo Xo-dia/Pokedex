275d15de0f60d703cad5cb2dd035a9ff72b005a6
Madzzino <fjldumele@gmail.com>
2025-01-22 23:32:12 +0100

    2h of learning

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,88 @@
 import React, {FunctionComponent} from 'react';
+import PokemonList from './page/pokemon-list';
+
+const App: FunctionComponent = () => { 
+
+
+    return (
+    <PokemonList/>
+    )
+}
+
+    export default App;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// const App: FunctionComponent = () => { 
+//     const [pokemons, setPokemons] = useState<Pokemon[]>([]);
+
+//     useEffect(() => {
+//         setPokemons(POKEMONS);
+//     }, []);
+
+//     return (
+//     <div>
+//         <h1>Pokédex</h1>
+//         <p>Il y a {pokemons.length} pokémons dans le pokedex</p>
+//     </div>
+//     )
+//     }
+
+//     export default App;
+
+
+// const App: FunctionComponent = () => { 
+//     const [pokemons] = useState<Pokemon[]>(POKEMONS);
+
+//      return (
+//       <div>
+//         <h1>Pokédex</h1>
+//         <p>Il y a {pokemons.length} pokémons dans le pokedex</p>
+//       </div>
+//      )
+    
+//     }
+      
+//     export default App;
+
+
+// const App: FunctionComponent = () => { // typé App en React.FC (function component) = composant que l'on peut ecrir sous la forme d'une fonction
+// //  const name: String = 'React';
+// const [name, setName] = useState<String>('React');
+// /* var nameStateVariable = useState('React');
+//     var name = nameStateVarible[0];
+//     var steName = nameStateVariable[1]; */
+//  return (
+//   <h1>Bonjour, {name} !</h1>
+//  )
+
+//  const [Pokemon, setName] = useState<>()
+
+// }
   
-const App: FunctionComponent = () => { // typé App en React.FC (function component) = composant quel l'on peut ecrir sous la forme d'un fonction
+// export default App;
+
+
+/*import React from 'react';
+  
+const App: React.FC = () => {
  const name: String = 'React';
     
  return (
@@ -8,4 +90,16 @@ const App: FunctionComponent = () => { // typé App en React.FC (function compon
  )
 }
   
-export default App;
+export default App;*/
+
+
+/* import React from 'react';
+
+ export default class App extends React.Component {
+    const name: string = "React";
+
+    render() {
+        return <h1> Hello, {name}<h1>;
+    }
+ }
+ */
